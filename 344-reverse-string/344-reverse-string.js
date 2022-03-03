df2f754f0cc6ee5740ecb6d86a54b9222d30ae10
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-03-03 21:58:30 +0530

    Time: 92 ms (93.40%), Space: 49.1 MB (50.14%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -3,5 +3,12 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-   return s.reverse(); 
+   let left = 0;
+   let right = s.length - 1;
+    
+   while(left <= right){
+     [s[left], s[right]] = [s[right], s[left]];
+     left++;
+     right--;  
+   } 
 };
