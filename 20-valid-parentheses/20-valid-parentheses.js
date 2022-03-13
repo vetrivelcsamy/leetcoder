741a7bfb3ee2961bbca47d91f3f47effaef2d873
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-03-13 22:49:16 +0530

    Time: 78 ms (67.48%), Space: 42.4 MB (37.93%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,27 +3,22 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-   let buffer = []; 
-   
-   if(s.length % 2 === 1){
-     return false;  
-   } 
+   let buffer = [];
     
+    if(s.length % 2 !== 0){
+      return false;  
+    }
     
-  for(let i = 0; i < s.length; i++){
-      
-    if(s[i] === '('){
-       buffer.push(')'); 
-        
-    }else if(s[i] === '{'){
-      buffer.push('}');  
-        
-    }else if(s[i] === '['){
-      buffer.push(']'); 
-        
-    } else if(!buffer.length || s[i] !== buffer.pop()){
-      return false;       
-    }  
-  }
-  return !buffer.length;
+    for(let i = 0; i < s.length; i++){
+       if(s[i] === '('){
+         buffer.push(')'); 
+       }else if(s[i] === '{'){
+         buffer.push('}'); 
+       }else if(s[i] === '['){
+         buffer.push(']'); 
+       }else if(buffer.length === 0 || s[i] !== buffer.pop()){
+         return false   
+       }  
+    }
+    return buffer.length === 0;
 };
