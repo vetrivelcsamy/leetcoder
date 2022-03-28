e68ce0d705e92c09530a097368b9278b4f5d7a3f
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-03-28 23:13:14 +0530

    Time: 110 ms (50.67%), Space: 43.9 MB (39.44%) - LeetHub

--- 69-sqrtx/69-sqrtx.js
+++ 69-sqrtx/69-sqrtx.js
@@ -3,18 +3,20 @@
  * @return {number}
  */
 var mySqrt = function(x) {
- let left = 0;
- let right = x;
+  let left = 0;
+  let right = x;
+  let ans = -1;
     
   while(left <= right){
     const mid = left + Math.floor((right - left) / 2);
     const sum = mid * mid;
       
-     if(sum <= x){
-        left = mid + 1;
-     } else{
-       right = mid - 1;  
-     }
+    if(sum <= x){
+      ans = mid;  
+      left = mid + 1;  
+    } else{
+      right = mid - 1;  
+    }  
   }
-    return right;
+  return ans;  
 };
