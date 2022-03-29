d54e13d6b978a9478050971803ad12fd9526043e
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-03-29 22:22:26 +0530

    Time: 131 ms (11.74%), Space: 42.5 MB (84.15%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -4,12 +4,19 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-  for(let i = 0; i < numbers.length; i++){
-    for(let j = i + 1; j < numbers.length; j++){
-      if(numbers[i] + numbers[j] === target){
-       return [i + 1, j + 1];   
-      }   
-    }  
-  }
+  let left = 0;
+  let right = numbers.length - 1;
+    
+   while(left < right){
+    let sum = numbers[left] + numbers[right];
+    
+    if(sum > target){
+     right--;   
+    } else if(sum < target){
+     left++;   
+    }else{
+     return [left + 1, right + 1];   
+    }   
+   }
   return [-1, -1];  
 };
