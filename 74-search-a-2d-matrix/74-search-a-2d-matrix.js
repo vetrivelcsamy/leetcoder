140cfcf38bdb006d7a60a474a4b4b6e61c69ff99
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-03-30 10:02:36 +0530

    Time: 81 ms (52.02%), Space: 42.1 MB (76.69%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    for(let i = 0; i < matrix.length; i++){
+      for(let k = 0; k < matrix[i].length; k++){
+        if(matrix[i][k] === target){
+           return true; 
+        }  
+      }  
+    }
+    return false;
+};
