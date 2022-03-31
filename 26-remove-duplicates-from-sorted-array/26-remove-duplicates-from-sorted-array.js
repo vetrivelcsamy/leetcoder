7a6425ec47120774f576e7acc7e57d87153c5001
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-03-31 23:49:18 +0530

    Time: 97 ms (67.11%), Space: 44.8 MB (55.49%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let buffer = 0;
-    for(let i  = 0; i < nums.length; i++){
-       if(nums[i - 1] !== nums[i]){
-         nums[buffer++] = nums[i]  
-       }  
-    }
-    return buffer;
+   let buffer = 0; 
+   for(let i = 0; i < nums.length; i++){
+     if(nums[i] !== nums[i + 1]){
+       nums[buffer++] = nums[i];  
+     }  
+   }
+   return buffer; 
 };
