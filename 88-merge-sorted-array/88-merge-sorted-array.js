b2cf82ea383a395882903f4464795a662a9690b2
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-04-06 23:00:14 +0530

    Time: 62 ms (87.15%), Space: 42 MB (79.53%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.js
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -6,8 +6,8 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    for(let i = 0; i !== n; i++){  
-      nums1[i + m] = nums2[i];  
-    }
-    nums1.sort((a, b) => a - b);
+  for(let i = 0; i !== n; i++){
+   nums1[m + i] = nums2[i];   
+  }
+  return nums1.sort((a, b) => a - b);  
 };
