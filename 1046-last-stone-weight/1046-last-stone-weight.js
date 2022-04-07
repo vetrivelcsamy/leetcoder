e3aa796a1b8f185d4a9d9deffa6ffb4f65f9ce3f
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-04-07 22:29:52 +0530

    Time: 97 ms (32.06%), Space: 42.7 MB (71.76%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function(stones) {
+      const pq = new MaxPriorityQueue();
+    for (const stone of stones) {
+        pq.enqueue('x', stone);
+    }
+    
+    while (pq.size() > 1) {
+        const a = pq.dequeue()['priority'];
+        const b = pq.dequeue()['priority'];
+        if (a > b) {
+            pq.enqueue('x', a - b);
+        }
+    }
+    return pq.isEmpty() ? 0 : pq.dequeue()['priority']; 
+};
