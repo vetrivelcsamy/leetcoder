ab7c137f0ea9cc4b12187a5107f880a3755b17c1
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-04-08 23:15:16 +0530

    Time: 74 ms (78.38%), Space: 45.8 MB (17.63%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -3,19 +3,6 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-  let majority = nums[0];
-  let count = 0;
-   
-  for(let i = 0; i < nums.length; i++){
-    if(count === 0){
-      majority = nums[i];   
-    }
-    
-    if(nums[i] === majority){
-      count++;  
-     } else{
-      count--;  
-    }  
-  }
- return majority;
+    nums.sort((a, b) => a - b);
+    return nums[Math.floor(nums.length / 2)];
 };
