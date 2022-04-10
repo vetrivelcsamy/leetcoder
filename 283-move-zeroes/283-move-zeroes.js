522db56718f58c163eef1f1d00b6a89f973082f1
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-04-10 21:40:40 +0530

    Time: 99 ms (80.26%), Space: 46.8 MB (53.41%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -2,16 +2,16 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function (nums) {
-  let left = 0;
-  let right = 0;
-  let n = nums.length;
-
-  while (right < n) {
-    if (nums[right] !== 0) {
-      [nums[right], nums[left]] = [nums[left], nums[right]];
-      left++;
-    }
-    right++;
+var moveZeroes = function(nums) {
+  let buffer = 0;
+    
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i] !== 0){
+      nums[buffer++] = nums[i];  
+    }  
   }
+    
+  for(let k = buffer; k < nums.length; k++){
+     nums[k] = 0; 
+  }  
 };
