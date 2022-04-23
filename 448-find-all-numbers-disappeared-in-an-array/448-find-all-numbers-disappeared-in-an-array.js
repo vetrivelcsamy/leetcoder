a481120dccaedc6495a1cfd439c4aad5fa1c2b70
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-04-23 22:46:55 +0530

    Time: 7683 ms (12.23%), Space: 49.8 MB (89.00%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.js
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.js
@@ -3,10 +3,10 @@
  * @return {number[]}
  */
 var findDisappearedNumbers = function(nums) {
-  let srt = nums.sort((a, b) => a - b);
+  //let srt = nums.sort((a, b) => a - b);
    let buffer = []; 
   for(let i = 1; i <= nums.length; i++){
-    if(!srt.includes(i)){
+    if(!nums.includes(i)){
       buffer.push(i);
     }   
   }
