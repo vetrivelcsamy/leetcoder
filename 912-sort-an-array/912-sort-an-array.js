c6c9b2e6f70dcce343670d55f51f6b0a73c674cf
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-05-08 23:18:03 +0530

    Time: 196 ms (67.49%), Space: 53.7 MB (63.12%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.js
+++ 912-sort-an-array/912-sort-an-array.js
@@ -3,15 +3,5 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-   for(let i = 0; i < nums.length; i++){
-     for(let k = i + 1; k < nums.length; k++){
-       if(nums[i] > nums[k]){
-         //[nums[i], nums[k]] = [nums[k], nums[i]]; 
-         let temp = nums[i];
-         nums[i] = nums[k];
-         nums[k] = temp;  
-       }  
-     }  
-   }
-  return nums;  
+   return nums.sort((a, b) => a - b);
 };
