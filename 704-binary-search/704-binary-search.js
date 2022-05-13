8a1f79bfaf9028e3abafbd448359c7ebd1a04e6a
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-05-13 23:30:45 +0530

    Time: 75 ms (69.07%), Space: 45.5 MB (14.84%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,19 +4,21 @@
  * @return {number}
  */
 var search = function(nums, target) {
-   let left = 0;
-   let right = nums.length - 1;
+  let left = 0;
+  let right = nums.length - 1;
     
-   while(left <= right){
-     const mid = left + Math.floor((right - left) / 2);
-       
-     if(nums[mid] === target){
-       return mid;  
-     } else if(nums[mid] > target){
-       right = mid - 1  
-     } else {
-       left = mid + 1;
-     }  
-   }
-   return -1; 
+  while(left <= right){
+   const mid = left + Math.floor((right - left) / 2);
+   
+    if(nums[mid] === target){
+      return mid;   
+    }
+    
+    if(nums[mid] > target){
+      right = mid - 1;  
+    }else{
+      left = mid + 1;  
+    }  
+  }
+  return -1;  
 };
