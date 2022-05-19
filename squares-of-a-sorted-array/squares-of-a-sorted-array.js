2893e195b1008aa3c0949882383171b6dc42c6fd
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-05-19 23:16:12 +0530

    Time: 142 ms, Memory: 48.9 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -4,7 +4,7 @@
  */
 var sortedSquares = function(nums) {
     for(let i = 0; i < nums.length; i++){
-      nums[i] = nums[i] * nums[i];
+      nums[i] = nums[i] * nums[i];  
     }
     return nums.sort((a, b) => a - b);
 };
