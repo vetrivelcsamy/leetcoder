ae266f6a2c3d8595c82bc7cf7aeb92bc667050b3
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-05-25 23:46:20 +0530

    Time: 118 ms (22.47%), Space: 41.6 MB (89.72%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -3,8 +3,9 @@
  * @return {number}
  */
 var fib = function(n) {
-  if(n <= 1){
-    return n;  
-  }
-  return fib(n - 1) + fib(n - 2);  
+  if(n < 2){
+   return n;   
+  } else{
+    return fib(n - 1) + fib(n - 2);  
+  }   
 };
