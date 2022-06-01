21e501df5cc7aefabe845e0e96432e44e946f841
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-06-01 21:24:21 +0530

    Time: 91 ms (35.52%), Space: 42.2 MB (82.11%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,10 +3,10 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-   let temp = 0;  
+  let temp = 0;
   for(let i = 0; i < nums.length; i++){
-     temp += nums[i];
-     nums[i] = temp; 
+    temp += nums[i];
+    nums[i] = temp; 
   }
   return nums;  
 };
