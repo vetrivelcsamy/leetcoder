4d9cc64fda472d3625a7a3988decf97880aa0b71
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-06-15 22:36:00 +0530

    Time: 175 ms (25.17%), Space: 42.3 MB (74.42%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,11 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  for(let i = 0; i < nums.length; i++){
-   for(let j = i + 1; j < nums.length; j++){
-     if(nums[i] + nums[j] === target){
-      return [i,j];   
+   for(let i = 0; i < nums.length; i++){
+     for(let k = i + 1; k < nums.length; k++){
+       if(nums[i] + nums[k] === target){
+         return [i, k];  
+       }  
      }  
-   }   
-  }  
+   } 
 };
