0801c3a9f905a3e27b558247869462a7f17b8a9f
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-06-25 22:58:02 +0530

    Time: 91 ms (33.38%), Space: 41.7 MB (68.69%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -3,14 +3,22 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-  let left = 0;
-   let q = 0; 
-  let right = 1;
- 
- for(let i = 1; i <= n; i++){
-   left = q;
-   q = right;
-   right = left + q;  
- } 
-  return right;  
+   let fn1 = 2;
+   let fn2 = 1;
+   let fn = 0;
+
+    if(n === 1){
+      return fn2;  
+    }
+    
+     if(n === 2){
+      return fn1;  
+    }
+    
+    for(let i = 3; i <= n; i++){
+      fn = fn2 + fn1;
+      fn2 = fn1;
+      fn1 = fn;  
+    }
+    return fn;
 };
