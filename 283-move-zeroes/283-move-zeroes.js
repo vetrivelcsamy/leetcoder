aeed3e93687516bf69b3cac1589f815a8debb2ae
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-06-26 22:53:00 +0530

    Time: 120 ms (65.14%), Space: 46.8 MB (38.58%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -3,18 +3,13 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
- // meesho interview question
-  let n = nums.length;
-  let left = 0;
-  let right = 0;
-    
-   while(right < n){
-     if(nums[right] !== 0){
-       let temp = nums[right];
-       nums[right] = nums[left];
-       nums[left] = temp;
-       left++ ; 
-     }
-      right++; 
-   } 
+  let insertPos = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] != 0) {
+      nums[insertPos++] = nums[i];
+} }
+  for (let j = insertPos; j < nums.length; j++) {
+    nums[j] = 0;
+}
+  return nums;  
 };
