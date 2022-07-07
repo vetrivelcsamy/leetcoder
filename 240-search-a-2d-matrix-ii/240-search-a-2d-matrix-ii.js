070037c152aa6c1d16f5a87077b2ca5711c80d36
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-07-07 23:48:21 +0530

    Time: 4746 ms (18.07%), Space: 44.8 MB (85.88%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.js
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.js
@@ -3,22 +3,22 @@
  * @param {number} target
  * @return {boolean}
  */
-var searchMatrix = function(matrix, target) {      
-  for(let i = 0; i < matrix.length; i++){
-   let left = 0;
-   let right = matrix[i].length - 1;
-      
-   while(left <= right){
-    const mid = left + Math.floor((right - left) >> 1);
-     
-    if(matrix[i][mid] === target){
-      return true;  
-     } else if(matrix[i][mid] > target){
-       right = mid - 1;  
-     } else{
-      left = mid + 1;  
-    }   
-   }   
-  }
-  return false;  
+var searchMatrix = function(matrix, target) {
+   for(let i = 0; i < matrix.length; i++){
+     let start = 0;
+     let end = matrix[i].length - 1;
+       
+     while(start <= end){
+       const mid = start + Math.floor((end - start) / 2);
+       
+       if(matrix[i][mid] === target){
+         return true;  
+       } else if(matrix[i][mid] > target){
+          end = mid - 1;   
+       }else{
+          start = mid + 1;  
+       }  
+     }  
+   }
+   return false; 
 };
