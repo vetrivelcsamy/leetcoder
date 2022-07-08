582623ced7ee80240b7eee4e860445d7d3b02cec
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-07-08 23:37:56 +0530

    Time: 99 ms (30.89%), Space: 42.2 MB (52.07%) - LeetHub

--- 75-sort-colors/75-sort-colors.js
+++ 75-sort-colors/75-sort-colors.js
@@ -3,5 +3,5 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-   return nums.sort((a, b) => a - b); 
+   return nums.sort((a,b) => a - b);
 };
