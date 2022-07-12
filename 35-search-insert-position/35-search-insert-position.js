2019c8ed8f7a8ae989e54a1216a03bc36d9d4bf3
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-07-12 23:55:02 +0530

    Time: 112 ms (11.66%), Space: 42.1 MB (58.22%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -4,18 +4,21 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-  let start = 0;
-  let end = nums.length - 1;
-  let ans = nums.length;  
-  while(start <= end){
-    const mid = start + Math.floor((end - start) >> 1);
-
-     if(nums[mid] >= target){
-      ans = mid;
-       end = mid - 1;  
-     }else{
-       start = mid + 1;  
-     }
-  }
-   return ans; 
+  let left = 0;
+   let right = nums.length - 1;
+    
+   while(left <= right){
+    const mid = left + Math.floor((right - left) / 2);
+    
+      if(nums[mid] === target){
+        return mid;  
+      } 
+      
+      if(nums[mid] > target){
+         right = mid - 1;  
+      } else{
+        left = mid + 1;  
+      } 
+   }
+  return left; 
 };
