b20c879b5c8cdc1f57b83bef35f49d24735e2041
Vetrivel Chinnasamy <vetrivelchinnasamy6@gmail.com>
2022-07-23 23:47:32 +0530

    Time: 107 ms (16.30%), Space: 42 MB (65.08%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.js
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -3,16 +3,6 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let index = s.length - 1;
-    while (s[index] === ' ') {
-        index--;
-    }
-    let wordLength = 0;
-    while (index >= 0 && s[index] !== ' ') {
-        wordLength++;
-        index--;
-    }
-    return wordLength;
-
- 
+  let spt = s.trim().split(' ');
+   return spt[spt.length - 1].length;
 };
